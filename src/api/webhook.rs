306f86e18ac191099a8b141177801b0c5cafaad9
Windfarer <windfarer@gmail.com>
2018-10-18 12:10:55 +0800

    rename field, send back

--- src/api/webhook.rs
+++ src/api/webhook.rs
@@ -2,11 +2,18 @@ use actix_web::{Json, Result};
 
 #[derive(Serialize, Deserialize)]
 pub struct Payload {
+    #[serde(rename = "ref")]
+    pub ref_: String,
     pub before: String,
     pub after: String,
+    pub created: bool,
+    pub deleted: bool,
+    pub forced: bool,
+    pub base_ref: Option<String>,
+    pub compare: String,    
 }
 
 
-pub fn webhook(payload: Json<Payload>) -> Result<String> {
-    Ok(format!("before {}", payload.before))
+pub fn webhook(payload: Json<Payload>) -> Result<Json<Payload>> {
+    Ok(payload)
 }
