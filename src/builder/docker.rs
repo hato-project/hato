8b484c6521c84769e6cc6e39d3481614435c1191
Windfarer <windfarer@gmail.com>
2018-10-24 20:56:40 +0800

    build and push

--- src/builder/docker.rs
+++ src/builder/docker.rs
@@ -1,12 +1,43 @@
 use std::process::Command;
 
-// Command::new("git").arg("clone").arg("your repo addr")
-// Command::new("docker").arg("--rm").arg("--tag").arg("your target image tag").arg(".")
-
 pub fn run_command() {
     let output = Command::new("ls")
         .arg("-l")
         .arg("-a")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+        print!("rustc succeeded and stdout was:\n{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+
+        print!("rustc failed and stderr was:\n{}", s);
+    }
+}
+
+pub fn build_image(image_tagname: String) {
+    let output = Command::new("docker")
+        .arg("--rm")
+        .arg("--tag")
+        .arg(image_tagname)
+        .arg(".")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+        print!("rustc succeeded and stdout was:\n{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+
+        print!("rustc failed and stderr was:\n{}", s);
+    }
+}
+
+pub fn push_image(image_tagname: String) {
+    let output = Command::new("docker")
+        .arg("push")
+        .arg(image_tagname)
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
     if output.status.success() {
