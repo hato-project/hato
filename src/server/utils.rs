cf36d4a6380c03450947137964199f3880490e10
Windfarer <windfarer@gmail.com>
2018-12-24 11:45:45 +0800

    static

--- src/server/utils.rs
+++ src/server/utils.rs
@@ -3,10 +3,12 @@ use bcrypt::{hash, verify};
 
 use std::env;
 
+static DEFAULT_HASH_COST: u32 = 10;
+
 pub fn hash_password(password: &str) -> String {
     let hash_cost: u32 = match env::var("HASH_COST") {
-        Ok(cost) => cost.parse().unwrap_or(10),
-        _ => 10,
+        Ok(cost) => cost.parse().unwrap_or(DEFAULT_HASH_COST),
+        _ => DEFAULT_HASH_COST,
     };
     hash(password, hash_cost).unwrap()
 }
