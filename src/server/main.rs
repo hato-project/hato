156b4479bb4b6f283349eb95d8db85104b2c9987
Windfarer <windfarer@gmail.com>
2019-01-17 16:26:13 +0800

    lazy static

--- src/server/main.rs
+++ src/server/main.rs
@@ -12,6 +12,8 @@ extern crate log;
 extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
 
 extern crate actix_web;
 extern crate bcrypt;
--- src/server/utils.rs
+++ src/server/utils.rs
@@ -15,12 +15,17 @@ struct Claims {
 
 static DEFAULT_HASH_COST: u32 = 10;
 
-pub fn hash_password(password: &str) -> String {
-    let hash_cost: u32 = match env::var("HASH_COST") {
-        Ok(cost) => cost.parse().unwrap_or(DEFAULT_HASH_COST),
-        _ => DEFAULT_HASH_COST,
+lazy_static! {
+    static ref HASH_COST: u32 = {
+        match env::var("HASH_COST") {
+            Ok(cost) => cost.parse().unwrap_or(DEFAULT_HASH_COST),
+            _ => DEFAULT_HASH_COST,
+        }
     };
-    hash(password, hash_cost).unwrap()
+}
+
+pub fn hash_password(password: &str) -> String {
+    hash(password, *HASH_COST).unwrap()
 }
 
 pub fn verify_password(password: &str, hashed: &str) -> bool {
